514016137b4279760c35cdb845389481e2a1e9cc
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-11-09 14:23:53 -0800

    Proximity to mine logic, board segment identifier.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 /*----- constants -----*/
 
+//only in const for now, refactor to calculate based on changing grid eventually
+let corners = ['1', '10', '91', '100']
+let tEdge = ['2', '3', '4', '5', '6', '7', '8', '9']
+let rEdge = ['20', '30', '40', '50', '60', '70', '80', '90']
+let lEdge = ['11', '21', '31', '41', '51', '61', '71', '81']
+let bEdge = ['92', '93', '94', '95', '96', '97', '98', '99']
 
 
 /*----- app's state (variables) -----*/
@@ -7,6 +13,8 @@
 let alreadyClicked;
 let mines;
 let proximous;
+let empties;
+
 
 /*----- cached element references -----*/
 
@@ -37,15 +45,18 @@ function handleClick (evt) {
 
 //---------------------------------------------------------secondary functions here
 //any function that plugs directly into the clickHandler (as few as possible)
-//resets gameplay
+//ONLY RUNS ON INIT
 function init () {
     resetState();
-    plantMines();
+    resetBoard();
+    getMines();
+    getProximous();
     render();
 }
-//during gameplay
+//RUNS DURING GAMEPLAY
 function render () {
     renderCells();
+    //rendermessage
 }
 //---------------------------------------------------------------tertiary functions
 //-----------------------------------------ONLY RUNS ON INIT
@@ -53,27 +64,58 @@ function render () {
 function resetState() {
     alreadyClicked = [];
     mines = [];
+    proximous = [];
+    empties = [];
 }
-
-function plantMines () {
+function resetBoard () {
+    for (i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = '';
+    }
+}
+function getMines () {
     //i<x x is how many mines to plant! later change based on diff selected
     for (i =1; i < 11; i++) {
         mines.push(getRandomInt(1, 100));
     }
     console.log(mines);
 }
-
-//simply generates a random number for plantMines fx above
+//simply generates a random number for getMines fx above
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+//need to evaluate surrounding cells, if cell is on the edge, evaluate only actually adjacent,
+//with varying dif, refactor to cal corners and edges on it's own, then run evaluations
+function getProximous(cell) {
+    for (i = 0; i < cells.length; i++) {
+        //corner cases
+        if (cells[i].id === "1") {
+            //cells[i].innerHTML = 'C1';
+        } else if (cells[i].id === '10') {
+            //cells[i].innerHTML = 'C2';
+        } else if (cells[i].id === '91') {
+            //cells[i].innerHTML = 'C3';
+        } else if (cells[i].id === '100') {
+            //cells[i].innerHTML = 'C4';
+        } else if (tEdge.includes(cells[i].id)) {
+            //cells[i].innerHTML = 'TE';
+        } else if (rEdge.includes(cells[i].id)) {
+            //cells[i].innerHTML = 'RE';
+        } else if (bEdge.includes(cells[i].id)) {
+            //cells[i].innerHTML = 'BE';
+        } else if (lEdge.includes(cells[i].id)) {
+            //cells[i].innerHTML = 'LE';
+        } else {
+            //cells[i].innerHTML = 'mid';
+        }
+    
+    }
+
 }
 
 //----------------------------------------RUNS DURING GAMEPLAY
 function renderCells () {
     //resets the html at each click
-    for (i = 0; i < cells.length; i++) {
-        cells[i].innerHTML = '';
-    }
     for (i = 0; i < cells.length; i++) {
         //need to also preclude mines from showing on hidden squares && alreadyClicked.includes(parseInt(cells[i].id))
         if (cells[i].nodeName.toLowerCase() === 'div' && mines.includes(parseInt(cells[i].id))) {
