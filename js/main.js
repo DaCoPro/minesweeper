e777e0e81e79f0dfdefab19d63ac386ef3f4b2e3
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-11-08 19:34:25 -0800

    Notes. Some state. Some reset.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@
 
 /*----- app's state (variables) -----*/
 
-
+let alreadyClicked = [];
+let hiddenCells = [...Array(100).keys()];
+let visibleCells = [];
 
 /*----- cached element references -----*/
 
@@ -16,24 +18,35 @@ document.getElementById('board').addEventListener('click', handleClick);
 document.getElementById('reset').addEventListener('click', handleClick);
 
 /*----- functions -----*/
-
 //-------------------------------------------------------------main functions here
 
 function handleClick (evt) {
+    //if this, bail (s)
+    if (alreadyClicked.includes(evt.target.id))
+        if (evt.target.id !== 'reset') return;
     if (evt.target.id === 'board') return;
     if (evt.target.id === 'reset') init();
     console.log(evt.target.id);
+    alreadyClicked.push(evt.target.id);
 }
-
-
-
-
-
-
 
 //---------------------------------------------------------secondary functions here
 
 function init () {
-    console.log("Init isn't built yet!");
+    console.log("Init is under construction!");
+    //reset all state
+    alreadyClicked = [];
+
+    //set all to hidden
+    hiddenCells = [...Array(100).keys()];
 }
 
+function renderBoard () {
+    //set all to hidden
+}
+
+//---------------------------------------------------------------tertiary functions
+
+function hideBoard () {
+    console.log("hideBoard is under construction!");
+}
