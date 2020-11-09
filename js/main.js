62e4bdf1867a499f4aa776cc5d970aff53d9ba5a
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-11-08 20:40:29 -0800

    Add mines in state only.

--- js/main.js
+++ js/main.js
@@ -5,12 +5,11 @@
 /*----- app's state (variables) -----*/
 
 let alreadyClicked = [];
-let hiddenCells = [...Array(100).keys()];
-let visibleCells = [];
+let mines = [];
 
 /*----- cached element references -----*/
 
-
+const cells = 
 
 /*----- event listeners -----*/
 
@@ -19,34 +18,54 @@ document.getElementById('reset').addEventListener('click', handleClick);
 
 /*----- functions -----*/
 //-------------------------------------------------------------main functions here
+//only allows certain clicks (X)
+//updates state              ( )
+//renders board              (X)
+ 
+init();
 
 function handleClick (evt) {
-    //if this, bail (s)
-    if (alreadyClicked.includes(evt.target.id))
-        if (evt.target.id !== 'reset') return;
+    //only allow certain clicks
+    if (alreadyClicked.includes(parseInt(evt.target.id))) 
+        if (evt.target.id !== "reset") return;
     if (evt.target.id === 'board') return;
     if (evt.target.id === 'reset') init();
-    console.log(evt.target.id);
-    alreadyClicked.push(evt.target.id);
+    //console.log(evt.target.id);
+
+    //updates state
+    alreadyClicked.push(parseInt(evt.target.id));
+    
+    renderBoard();
 }
 
 //---------------------------------------------------------secondary functions here
+//any function that plugs directly into the clickHandler (as few as possible)
 
 function init () {
     console.log("Init is under construction!");
-    //reset all state
-    alreadyClicked = [];
-
-    //set all to hidden
-    hiddenCells = [...Array(100).keys()];
+    resetState();
+    plantMines();
 }
 
 function renderBoard () {
-    //set all to hidden
+    
 }
-
+    
 //---------------------------------------------------------------tertiary functions
 
-function hideBoard () {
-    console.log("hideBoard is under construction!");
+function resetState() {
+    alreadyClicked = [];
+    mines = [];
 }
+
+function plantMines () {
+    for (i =1; i < 9; i++) {
+        mines.push(getRandomInt(1, 100));
+    }
+    //console.log(mines);
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+    
